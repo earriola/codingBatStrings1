8f95de7404024939bfacfba07d5f2bcaaf12a51f
Emmanuel Arriola <earriola5@stu.ctcd.edu>
2017-04-24 17:02:02 -0500

    Solved makeTags

--- driver.cpp
+++ driver.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cassert>
 using namespace std;
+
+string makeTags(string one, string two);
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	assert(makeTags("i", "Yay") == "<i>Yay</i>");
+	assert(makeTags("i", "Hello") == "<i>Hello</i>");
+	assert(makeTags("cite", "Yay") == "<cite>Yay</cite>");
+	assert(makeTags("g", "Micheal") == "<g>Micheal</g>");
+	assert(makeTags("a", "Emmanuel") == "<a>Emmanuel</a>");
 	
 	return 0;
-}	
+}
+
+string makeTags(string one, string two)
+{
+	string results;
+	
+	results = "<" + one + ">" + two + "</" + one + ">";
+
+	return results;
+}
